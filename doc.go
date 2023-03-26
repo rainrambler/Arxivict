32d7a62dcd854201a47bcb91b1934fc87e05331a
rainrambler <wanganyu@outlook.com>
2023-03-26 14:22:16 +0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -9,25 +9,47 @@ type Version struct {
 	created string `json:"created"`
 }
 
+type Versions struct {
+	allVersions []*Version
+}
+
+func (p *Versions) Add(ver *Version) {
+	if ver == nil {
+		return
+	}
+	p.allVersions = append(p.allVersions, ver)
+}
+
 type Author struct {
 	names []string
 }
 
+type Authors struct {
+	allItems []*Author
+}
+
+func (p *Authors) Add(item *Author) {
+	if item == nil {
+		return
+	}
+	p.allItems = append(p.allItems, item)
+}
+
 type ArxivPaper struct {
-	id             string    `json:"id"`
-	submitter      string    `json:"submitter"`
-	authors        string    `json:"authors"`
-	title          string    `json:"title"`
-	comments       string    `json:"comments,omitempty"`
-	journalref     string    `json:"journal-ref,omitempty"`
-	doi            string    `json:"doi,omitempty"`
-	reportno       string    `json:"report-no,omitempty"`
-	categories     string    `json:"categories"`
-	license        string    `json:"license,omitempty"`
-	abstract       string    `json:"abstract"`
-	versions       []Version `json:"versions"`
-	updatedate     string    `json:"update_date"`
-	authors_parsed []Author  `json:"authors_parsed"`
+	id             string
+	submitter      string
+	authors        string
+	title          string
+	comments       string
+	journalref     string
+	doi            string
+	reportno       string
+	categories     string
+	license        string
+	abstract       string
+	versions       *Versions
+	updatedate     string
+	authors_parsed *Authors
 }
 
 type ArxivPapers struct {
@@ -42,7 +64,121 @@ func (p *ArxivPapers) ReadFile(filename string) {
 		return
 	}
 
-	log.Printf("%+v\n", js)
+	p.convert(js)
+	//log.Printf("%+v\n", js)
+}
+
+func (p *ArxivPapers) convert(jc *JsonContent) {
+	content := jc.Data
+	papers := []*ArxivPaper{}
+	arr := content.([]interface{})
+	for _, v := range arr {
+		paper := convPaper(v)
+		if paper != nil {
+			papers = append(papers, paper)
+
+			//log.Printf("[DBG]Add paper: %+v\n", *paper)
+		}
+	}
+	//log.Printf("Content: %v\n", content)
+	log.Printf("Total %d papers.\n", len(papers))
+}
+
+func convPaper(content interface{}) *ArxivPaper {
+	paper := new(ArxivPaper)
+	varmap := content.(map[string]interface{})
+	for k, v := range varmap {
+		switch k {
+		case "id":
+			paper.id = v.(string)
+		case "submitter":
+			paper.submitter = v.(string)
+		case "authors":
+			paper.authors = v.(string)
+		case "title":
+			paper.title = v.(string)
+		case "comments":
+			paper.comments = toNilOrString(v)
+		case "journal-ref":
+			paper.journalref = toNilOrString(v)
+		case "doi":
+			paper.doi = toNilOrString(v)
+		case "report-no":
+			paper.reportno = toNilOrString(v)
+		case "categories":
+			paper.categories = v.(string)
+		case "license":
+			paper.license = toNilOrString(v)
+		case "abstract":
+			paper.abstract = v.(string)
+		case "update_date":
+			paper.updatedate = v.(string)
+		case "versions":
+			paper.versions = convertVersions(v)
+		case "authors_parsed":
+			paper.authors_parsed = convertAuthorsParsed(v)
+		default:
+			log.Printf("[DBG][convPaper]Unknown [%v]:%v\n", k, v)
+		}
+	}
+	return paper
+}
+
+func convertVersions(content interface{}) *Versions {
+	var inst Versions
+	arr := content.([]interface{})
+	for _, v := range arr {
+		ver := convertVersion(v)
+		inst.Add(ver)
+	}
+
+	return &inst
+}
+
+func convertVersion(content interface{}) *Version {
+	var inst Version
+	varmap := content.(map[string]interface{})
+	for k, v := range varmap {
+		switch k {
+		case "version":
+			inst.version = v.(string)
+		case "created":
+			inst.created = v.(string)
+		default:
+			log.Printf("[DBG][convertVersion]Unknown [%v]:%v\n", k, v)
+		}
+	}
+
+	return &inst
+}
+
+func convertAuthorsParsed(content interface{}) *Authors {
+	var inst Authors
+	arr := content.([]interface{})
+	for _, v := range arr {
+		ver := convertAuthor(v)
+		inst.Add(ver)
+	}
+
+	return &inst
+}
+
+func convertAuthor(content interface{}) *Author {
+	var inst Author
+	arr := content.([]interface{})
+	for _, v := range arr {
+		inst.names = append(inst.names, v.(string))
+	}
+
+	return &inst
+}
+
+func toNilOrString(content interface{}) string {
+	if content == nil {
+		return ""
+	}
+
+	return content.(string)
 }
 
 func (p *ArxivPapers) PrintItems() {
