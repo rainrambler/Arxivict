0e8d3942afe596d1159fa9c9668343f68c2c12c3
rainrambler <wanganyu@outlook.com>
2024-11-03 09:37:56 +0800

    Update LogUtils.go

--- LogUtils.go
+++ LogUtils.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-	"path/filepath"
 	"time"
 )
 
@@ -13,18 +9,4 @@ func generateLogFileName() string {
 
 	datepart := t.Format("2006-01-02-15-04-05")
 	return "log" + datepart + ".txt"
-}
-
-func logSettings() {
-	logfilename := filepath.Join("log", generateLogFileName())
-	f, err := os.OpenFile(logfilename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	} else {
-		log.Printf("Log file %s created and opened!\n", logfilename)
-	}
-	defer f.Close()
-
-	mw := io.MultiWriter(os.Stdout, f)
-	log.SetOutput(mw)
 }
