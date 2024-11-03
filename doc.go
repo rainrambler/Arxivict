504a7e277d89254f20a0c083ade6473da935624a
rainrambler <wanganyu@outlook.com>
2024-11-03 17:15:29 +0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,13 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func SumKeywordsInTitle(filename string) {
 	var ap ArxivPapers
 	ap.ParseLargeFileByLine(filename)
 
 	desired := []string{"cs.LG", "cs.AI", "cs.CR", "cs.DB", "cs.IR"}
-	ap.stat.ToHtmlChart(desired)
+	ap.stat.ToHtmlChartPeriod(desired, 2015, 2024)
 	//ap.stat.PrintResult()
 	//ap.PrintResults()
 	//ap.PrintItems()
 
+	fmt.Println("======================")
+	ap.PrintResults()
 }
