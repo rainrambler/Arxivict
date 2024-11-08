37b9402f1db73d5fcadebfa48351f2f9a4cee43e
rainrambler <wanganyu@outlook.com>
2024-11-08 10:19:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
-// Arxivict project main.go
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
 func main() {
-	var ap ArxivPapers
-	ap.ReadFile(`arxivdemo.json`)
-	ap.PrintItems()
+	//logSettings()
+	logfilename := filepath.Join("log", generateLogFileName())
+	f, err := os.OpenFile(logfilename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	} else {
+		log.Printf("Log file %s created and opened!\n", logfilename)
+	}
+	defer f.Close()
+
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
+
+	log.Printf("INFO: Program started at %v\n", time.Now())
+
+	//SumKeywordsInTitle(`arxiv-metadata-oai-snapshot.json`)
+
+	CreateWordCloud(`arxiv-metadata-oai-snapshot.json`, "cs.AI")
+
+	log.Printf("INFO: Program ended at %v\n", time.Now())
 }
