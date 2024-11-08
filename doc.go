3ee0ae3d467ff58caef9ddb85deef7d3655f8043
rainrambler <wanganyu@outlook.com>
2024-11-08 10:18:51 +0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -17,3 +17,12 @@ func SumKeywordsInTitle(filename string) {
 	fmt.Println("======================")
 	ap.PrintResults()
 }
+
+func CreateWordCloud(filename, category string) {
+	var ap ArxivPapers
+	ap.SetCategories([]string{category})
+	ap.ParseLargeFileByLine(filename)
+	ap.GenWordCloud(filename, category)
+
+	//ap.stat.ToHtmlChartPeriod(desired, 2015, 2024)
+}
